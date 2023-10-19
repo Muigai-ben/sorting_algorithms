4ab3098016aea8b08c150980f495efc97c223e1e
Muigai-ben <muigaibenson37@gmail.com>
2023-10-19 14:06:16 -0700

    linked list

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,15 @@
+#include<sort.h>
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @nodes: Integer stored in the node
+ * @prev: poointer to the previous element of the list
+ * @next: pointer to the next element in the node
+ */
+
+void insertion_sort_list(listing_t **list)
+	listing_t *nodes, *prev, *next;
+	if (!*list || !(*list)->next)
+	return;
+	for nodes = (*list)->next; nodes != NULL; nodes=next
