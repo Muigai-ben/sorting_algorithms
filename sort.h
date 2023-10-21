c82275d4c7a383084113eac9cb0de2889f7e9605
stevembui52 <stevembui52@gmail.com>
2023-10-21 00:33:58 -0700

    a function that sorts an array of integers in ascending order using the Selection sort algorithm

--- sort.h
+++ sort.h
@@ -1,7 +1,11 @@
 #ifndef SORT_H
-#define SORT_H
-#include <stdlib.h>
+#define SOrt_H
+
+/* Built-in libraries */
 #include <stdio.h>
+#include <stdlib.h>
+
+/* Structs */
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -11,14 +15,17 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
-void print_list(const listint_t *list);
-void print_array(const int *array, size_t size);
+/* Prototypes */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
-#endif
+void quick_sort(int *array, size_t size);
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+#endif /* SORTING */
