94f0c130605ce18c18da857d3f32163d06421d8f
stevembui52 <stevembui52@gmail.com>
2023-10-23 06:13:28 -0700

    function that sorts a doubly linked list of integers in ascending order using the Insertion sort algorithm

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,15 +1,43 @@
-#include<sort.h>
-
+#include "sort.h"
 /**
- * struct listint_s - Doubly linked list node
- *
- * @nodes: Integer stored in the node
- * @prev: poointer to the previous element of the list
- * @next: pointer to the next element in the node
+ * insertion_sort_list - insertion sort list algorthms
+ * @list: list pointer
+ * Return: No Return
  */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *curr, *swap, *tmp;
 
-void insertion_sort_list(listing_t **list)
-	listing_t *nodes, *prev, *next;
-	if (!*list || !(*list)->next)
-	return;
-	for nodes = (*list)->next; nodes != NULL; nodes=next
+	if (!list || *list == NULL)
+		return;
+	curr = *list;
+	/* validate if there is only one element in list */
+	if (curr->next == NULL)
+		return;
+	while (curr->next != NULL)
+	{
+		swap = curr->next;
+		/* Comparison starts here */
+		if (curr->n > swap->n)
+		{
+			tmp = curr;
+			while (tmp != NULL && tmp->n > swap->n)
+			{
+				tmp->next = swap->next;
+				if (tmp->next != NULL)
+					tmp->next->prev = tmp;
+				swap->prev = tmp->prev;
+				if (swap->prev != NULL)
+					swap->prev->next = swap;
+				else
+					*list = swap;
+				tmp->prev = swap;
+				swap->next = tmp;
+				print_list(*list);
+				tmp = swap->prev;
+			}
+			continue;
+		}
+		curr = curr->next;
+	}
+}
